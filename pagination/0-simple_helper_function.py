c3b2cb1ba8727c198dc468255d6d612eebfd88c5
‘Roupies’. <maxime.naguet@gmail.com>
2025-08-12 15:59:39 +0200

    add docstrings

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,16 +2,19 @@
 
 """Simple helper function for pagination"""
 
+
 def index_range(page: int, page_size: int) -> tuple[int, int]:
-    """Simple helper function for pagination
+    """
+    Calculate the start and end indices for a given page and page size.
 
 Args:
     page (int): The page number (starting from 1)
     page_size (int): The number of items per page
 
 Returns:
-    A tuple containing the start and end indices of the items in the page
-    """
+    Tuple[int, int]: A tuple containing the start index
+    and end index for the given page.
+"""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
