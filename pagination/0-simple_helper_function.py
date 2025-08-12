9fe8e57f696a354c9c97f5fa110f895d451ea438
‘Roupies’. <maxime.naguet@gmail.com>
2025-08-12 15:50:45 +0200

    add more docstrings

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""Simple helper function for pagination"""
+
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """Simple helper function for pagination
 
