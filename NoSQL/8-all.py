413505a0f29b3f7594b40818d4187437faaca010
‘Roupies’. <maxime.naguet@gmail.com>
2025-09-08 13:50:02 +0200

    completion of task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+def list_all(mongo_collection):
+    """
+    Lists all documents in a MongoDB collection.
+
+    Args:
+        mongo_collection: A pymongo collection object.
+
+    Returns:
+        A list of documents (empty list if none).
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
