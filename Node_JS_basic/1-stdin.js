84f336509943edda4255e8cd8771edd655afa797
‘Roupies’. <maxime.naguet@gmail.com>
2025-09-08 09:42:06 +0200

    correction of somes linters

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (userInput) => {
-  process.stdout.write(`Your name is: ${userInput}`);
+process.stdin.on('data', (Input) => {
+  process.stdout.write(`Your name is: ${Input}`);
   process.stdout.write('This important software is now closing\n');
   process.exit();
 });
--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -5,39 +5,34 @@ const databasePath = process.argv[2];
 
 const app = express();
 
-// root route
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-// /students route
-app.get('/students', async (req, res) => {
+app.get('/students', (req, res) => {
   res.set('Content-Type', 'text/plain');
   const text = 'This is the list of our students\n';
 
-  // save log to restore later
   const originalLog = console.log;
 
-  // empty string to catch logs
   let output = '';
 
-  // temporary log to catch log messages and build answer
   console.log = (message) => { output += `${message}\n`; };
 
-  // await the answer to avoid having asynchronous response
-  try {
-    await countStudents(databasePath);
-    console.log = originalLog;
-    res.send(text + output.trim());
-  } catch (err) {
-    // restore original log and show error message
-    console.log = originalLog;
-    res.send('Cannot load the database');
-  }
+  countStudents(databasePath)
+  // restore original log and show answer
+    .then(() => {
+      console.log = originalLog;
+      res.send(text + output.trim());
+    })
+    .catch(() => {
+      // restore original log and error message
+      console.log = originalLog;
+      res.send('Cannot load the database');
+    });
 });
 
-// listen on port 1245
 app.listen(1245);
 
 module.exports = app;
