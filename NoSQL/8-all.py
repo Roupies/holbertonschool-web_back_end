e7812ff5d1e2dd52b743c6d559fea0ad8ebe98e4
‘Roupies’. <maxime.naguet@gmail.com>
2025-09-08 13:57:13 +0200

    forgot doctstring module

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+"""
+Module that lists all documents in a MongoDB collection.
+"""
 def list_all(mongo_collection):
     """
     Lists all documents in a MongoDB collection.
