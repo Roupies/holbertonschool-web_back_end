e7cc183abd31714bdd45a83af871af4a8ce5ccd3
‘Roupies’. <maxime.naguet@gmail.com>
2025-09-08 16:16:30 +0200

    fixed syntax

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,8 @@
-function getListStudentsids() {
+import getListStudents from './0-get_list_students';
+
+function getListStudentIds() {
     const students = getListStudents();
     return students.map((student) => student.id);
 }
 
-export default getListStudentsids;
+export default getListStudentIds;
