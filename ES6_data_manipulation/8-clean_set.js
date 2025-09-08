c77071fc511eaf2c7f91fb39891f2dce0f182367
‘Roupies’. <maxime.naguet@gmail.com>
2025-09-08 16:47:50 +0200

    completion of task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+function cleanSet(set, startString) {
+    if (typeof startString !== 'string' || startString.length === 0) {
+        return '';
+    }
+    const result = [];
+    for (const item of set) {
+        if (item.startsWith(startString)) {
+            result.push(item.slice(startString.length));
+        }
+    }
+    return result.join('-');
+}
+
+export default cleanSet;
